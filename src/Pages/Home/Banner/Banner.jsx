e8358f0201b81e076fc7d1ddf66186a895623b0a
sifat <Sifatami596@gmail.com>
2022-11-25 15:46:12 +0600

    navbar added

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,46 @@
+// import React from "react";
+// import img1 from "../../../assets/banner/banner1.jpg";
+// import img2 from "../../../assets/banner/banner2.jpg";
+// import img3 from "../../../assets/banner/banner3.jpg";
+
+// const Banner = () => {
+//     return (
+//         <div className="carousel container w-full my-16">
+//             <div id="slide1" className="carousel-item relative w-full">
+//                 <img src={img1} alt="banner img" className="w-full" />
+//                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+//                     <a href="#slide3" className="btn btn-circle">
+//                         ❮
+//                     </a>
+//                     <a href="#slide2" className="btn btn-circle">
+//                         ❯
+//                     </a>
+//                 </div>
+//             </div>
+//             <div id="slide2" className="carousel-item relative w-full">
+//                 <img src={img2} alt="banner img" className="w-full" />
+//                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+//                     <a href="#slide1" className="btn btn-circle">
+//                         ❮
+//                     </a>
+//                     <a href="#slide3" className="btn btn-circle">
+//                         ❯
+//                     </a>
+//                 </div>
+//             </div>
+//             <div id="slide3" className="carousel-item relative w-full">
+//                 <img src={img3} alt="banner img" className="w-full" />
+//                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+//                     <a href="#slide2" className="btn btn-circle">
+//                         ❮
+//                     </a>
+//                     <a href="#slide1" className="btn btn-circle">
+//                         ❯
+//                     </a>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Banner;
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+
+    daisyui: {
+        themes: [
+            {
+                resaleTheme: {
+                    primary: "#7f1d1d",
+                    secondary: "#ef4444",
+                    "base-200": "#0E1525",
+
+                    accent: "#3A4256",
+                    neutral: "#3D4451",
+                    info: "#1D283A",
+                    "base-100": "#0F1729",
+                },
+            },
+        ],
+    },
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+};

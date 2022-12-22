99d62d79a1e1f5ffa6c42041e7cd2f15c2c12980
sifat <Sifatami596@gmail.com>
2022-12-22 05:43:44 -0700

    banner caption added

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -2,17 +2,34 @@ import React from "react";
 import img1 from "../../../assets/banner/banner1.jpg";
 import img2 from "../../../assets/banner/banner2.jpg";
 import img3 from "../../../assets/banner/banner3.jpg";
+import "./Banner.css";
 
 const Banner = () => {
     return (
         <section className="container">
-            <div className="carousel w-full my-16">
+            <div className="carousel w-full relative my-16">
                 <div id="slide1" className="carousel-item relative w-full">
-                    <img
-                        src={img1}
-                        alt="banner img"
-                        className="w-full rounded-lg"
-                    />
+                    <div className="carousel-img">
+                        {" "}
+                        <img
+                            src={img1}
+                            alt="banner img"
+                            className="w-full rounded-lg"
+                        />
+                    </div>
+
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-24 top-1/4">
+                        <h1 className="text-3xl font-bold text-white">
+                            Buy Certified Pre-owned Gadgets <br />
+                            <span className="text-2xl font-semibold text-primary">
+                                Daily Deal, Free Shipping
+                            </span>
+                        </h1>
+                        <br />
+                        {/* <h1 ">
+                            
+                        </h1> */}
+                    </div>
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide3" className="btn btn-circle">
                             ❮
